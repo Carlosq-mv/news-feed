228f3c183313eb3efeb7f87f3f3ecbc18a37ae59
carlosq-mv <cquiroz7869@gmail.com>
2026-07-21 13:22:35 -0700

    add logic to read json file with feeds

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import logging
+import json
+import sys
+
+from rss_reader import get_rss, parse_rss
+from db import insert_article
 
 logging.basicConfig(
     level=logging.INFO,
@@ -7,3 +12,17 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+def load_feed_config(path: str) -> list[dict[str, str]]:
+    """Load from a JSON file the source and url into a python dict.
+    Args:
+        path: A string that represents that file path to json file
+              that contains the feeds.
+
+    Returns:
+        A list of dict with the source and url of feeds.
+    """
+    with open(path, "r") as f:
+        feeds = json.load(f)
+    return feeds
